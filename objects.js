a644261392ed2e2320c146bc7c2b59a955ca62d7
Tony Boswell <tboswell@kc.rr.com>
2017-11-11 00:36:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,9 @@
+// creates an object with the keyname artistName and value songTitle
 var playlist = {artistName: "songTitle"}
+
+// creates a function that adds the artistName: songTitle key-value pair to the
+// object, playlist
+function updatePlaylist(obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
